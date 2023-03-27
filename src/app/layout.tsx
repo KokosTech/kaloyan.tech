aa16ed64b9353032ba1bd367ab0a8c4fa4ffdeee
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-03-27 16:17:27 +0300

    [ADD] Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+
+import "./globals.css";
 
 export const metadata = {
-  title: 'Kaloyan Doychinov | Porfolio',
-  description: 'Porfolio of Kaloyan Doychinov, a software developer and a student at TUES',
-}
+  title: "Kaloyan Doychinov | Porfolio",
+  description:
+    "Porfolio of Kaloyan Doychinov, a software developer and a student at TUES",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
